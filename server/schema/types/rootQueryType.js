03fa2d93ac7a517782ae87c3765cb24b3ef6680c
cmdallas <self@chrisdallas.tech>
2019-06-16 20:12:46 -0700

    Add user root query

--- server/schema/types/rootQueryType.js
+++ server/schema/types/rootQueryType.js
@@ -1,11 +1,17 @@
 const graphql = require('graphql');
+const UserType = require('./user');
 
 const { GraphQLObjectType, GraphQLID } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
   }
 });
 
